fda3bb590788c8cfc546bc447a588797dd8c8a9e
Lukas Lehrmann <121244965+leoLule@users.noreply.github.com>
2023-09-17 16:58:53 +0300

    first commit  chanage file structure for server

--- routes/scoresRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const scoreControllers = require('../controllers/scoreControllers');
-
-router.post('/newscore', scoreControllers.createNewScoreController);
-
-module.exports = router;
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const userControllers = require('../controllers/userControllers');
-
-router.post('/createuser', userControllers.createNewUserController);
-
-module.exports = router;
