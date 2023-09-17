659736af61cca98a8a4ea0ea9b49399273a1e797
Lukas Lehrmann <121244965+leoLule@users.noreply.github.com>
2023-09-17 19:21:50 +0300

    added logci for sign up but does not work yet

---
+++ middleware/usersMiddleware.js
@@ -0,0 +1,82 @@
+const { getUserByEmailModel } = require('../models/userModels');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+function passwordMatch(req, res, next) {
+  if (req.body.password !== req.body.rePassword) {
+    return res.status(400).send('Passwords dont match');
+  }
+  next();
+}
+
+async function isNewUser(req, res, next) {
+  const user = await getUserByEmailModel(req.body.email);
+  if (user) {
+    return res.status(400).send('User with this email already exists');
+  }
+  next();
+}
+
+async function isExistingUser(req, res, next) {
+  const user = await getUserByEmailModel(req.body.loginEmail);
+  // console.log(req.body);
+  if (user) {
+    req.body.user = user;
+    // console.log(req.body);
+    next();
+  } else {
+    res.status(400).send('User with this email does not exist');
+  }
+}
+
+function hashPwd(req, res, next) {
+  const saltRounds = 10;
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    if (err) {
+      res.status(400).send('Error hashing password');
+    } else {
+      req.body.password = hash;
+      next();
+    }
+  });
+}
+
+function auth(req, res, next) {
+  if (!req.headers.authorization) {
+    return res.status(401).send('Authorization headers required');
+  }
+  const token = req.headers.authorization.replace('Bearer ', '');
+
+  jwt.verify(token, process.env.TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send('Invalid token');
+    }
+    console.log(decoded);
+    req.body.ownerId = decoded.id;
+    next();
+  });
+}
+
+//might not need it
+function checkIfAdmin(req, res, next) {
+  const { token } = req.cookies;
+  jwt.verify(token, process.env.TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send('Invalid token');
+    }
+    if (decoded.isAdmin) {
+      req.body.isAdmin = decoded.isAdmin;
+      next();
+    } else {
+      res.status(403).send('You do not have permission');
+    }
+  });
+}
+module.exports = {
+  passwordMatch,
+  isNewUser,
+  hashPwd,
+  isExistingUser,
+  auth,
+  checkIfAdmin,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 const userControllers = require('../controllers/userControllers');
+const {
+  passwordMatch,
+  isNewUser,
+  hashPwd,
+  isExistingUser, // for login
+  auth, //might not need auth
+} = require('../middleware/usersMiddleware');
 
+//jessie's test route
 router.post('/createuser', userControllers.createNewUserController);
+
+//lukas's signup
+router.post(
+  '/signup',
+  /*validateBody(signupSchema)*/ passwordMatch,
+  isNewUser,
+  hashPwd,
+  userControllers.signup
+);
+
+// login
+// router.post(
+//   '/login',
+//   /*validateBody(signupSchema)*/ isExistingUser,
+//   userControllers.login
+// );
 
 module.exports = router;
