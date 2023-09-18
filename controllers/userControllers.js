f5ecc2b6ac91d0ee6000de45b47524b1425f8844
Lukas Lehrmann <121244965+leoLule@users.noreply.github.com>
2023-09-18 11:23:51 +0300

    signup works with insomnia

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -15,19 +15,52 @@ const createNewUserController = async (req, res) => {
   }
 };
 async function signup(req, res) {
-  // console.log(req.body);
+  console.log(req.body);
   try {
     const newUser = {
-      username: req.body.username,
+      userName: req.body.userName,
       email: req.body.email,
       password: req.body.password,
       rePassword: req.body.rePassword,
+      highScore: req.body.highScore,
+      isAdmin: req.body.isAdmin,
     };
 
     const id = await addUserModel(newUser);
     if (id) {
       res.send({ ok: true, id: id });
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+}
+async function login(req, res) {
+  console.log(req.body.loginPassword); //loginFormHolder.password} from FE
+  const { loginPassword, user } = req.body;
+
+  try {
+    bcrypt.compare(loginPassword, user.password, (err, result) => {
+      if (!result) {
+        return res.status(401).send('Incorrect password');
+      }
+      if (err) {
+        return res.status(500).send('Error comparing');
+      }
+
+      if (result) {
+        console.log(user);
+
+        const token = jwt.sign({ id: user.userId }, process.env.TOKEN_KEY, {
+          expiresIn: '999h',
+        });
+
+        res.send({
+          token: token,
+        });
+      }
+    });
+    // res.send("login success");
   } catch (error) {
     console.log(error);
     res.status(500).send(error.message);
